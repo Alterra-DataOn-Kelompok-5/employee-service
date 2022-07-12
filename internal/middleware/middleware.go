fc16d6d667ed78248413ae65fab06a595834214f
azkafr92 <azkafr92@gmail.com>
2022-07-12 18:24:05 +0700

    fix: write log to stdout

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,38 +1,33 @@
 package middleware
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	"github.com/Alterra-DataOn-Kelompok-5/employee-service/internal/dto"
-	"github.com/Alterra-DataOn-Kelompok-5/employee-service/pkg/util"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func LogMiddlewares(e *echo.Echo) {
-	dirname, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dirname)
+	// dirname, err := os.UserHomeDir()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(dirname)
 
-	path := dirname + "/logs/"
+	// path := dirname + "/logs/"
 
-	err = os.MkdirAll(path, os.ModePerm)
-	if err != nil {
-		log.Println(err)
-	}
-	fileName := path + util.Getenv("LOG_FILE", "employee-service.logs")
-	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic(fmt.Sprintf("error opening file: %v", err))
-	}
+	// err = os.MkdirAll(path, os.ModePerm)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// fileName := path + util.Getenv("LOG_FILE", "employee-service.logs")
+	// f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	panic(fmt.Sprintf("error opening file: %v", err))
+	// }
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format:           `[${time_rfc3339}] ${status} ${method} ${host}${uri} ${latency_human}` + "\n",
 		CustomTimeFormat: "2006/01/02 15:04:05",
-		Output:           f,
+		// Output:           f,
 	}))
 }
 
